7dfc89845b52f77b631d0a5e3d2cd3594e25ab7c
Michael Barnes <michael@blazingchilli.com>
2016-04-19 11:46:20 +0200

    Added the react image for demo purposes

--- src/app.jsx
+++ src/app.jsx
@@ -2,27 +2,41 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var ImageContainer = require('./image-container');
+
+var images = {
+  images: [
+    "https://cdn.auth0.com/blog/react-js/react.png"
+  ]
+};
+
 var App = React.createClass({
   render: function() {
-    return <div className="row panel panel-default">
-      <div className="panel-heading">
-      </div>
-      <div className="col-md-8 col-md-offset-2">
-        <h2 classname="text-center">
-          react-boiler
-        </h2>
-        <p className="lead">
-          You have made it, you now have things set up.
-        </p>
-        <p>
-          Visit <a href="">the repo</a> for more info or if there are any question please contact me on barnesmike@outlook.com
-        </p>
-      </div>
-      <div class="col-md-4">.
+
+    var images = this.props.images.map(function(image){
+      return <ImageContainer imageSource={image}/>
+    });
+
+    return <div className="container">
+      <div className="row panel panel-default">
+        <div className="panel-heading">
+        </div>
+        <div className="col-md-8 col-md-offset-2">
+          <h2 classname="text-center">
+            react-boiler
+          </h2>
+          <p className="lead">
+            You have made it, you now have things set up.
+          </p>
+          <p>
+            Visit <a href="https://github.com/michaelbarnes/react-boiler">the repo</a> for more info or if there are any question please contact me on barnesmike@outlook.com
+          </p>
+        </div>
+        <div className="row">{images}</div>
       </div>
     </div>
   }
 });
 
-var element = React.createElement(App, {});
+var element = React.createElement(App, images);
 ReactDOM.render(element, document.querySelector('.container'));
